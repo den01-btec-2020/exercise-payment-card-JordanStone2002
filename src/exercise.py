6475f73cf50958587400d4afeab1162952e8a77b
Jordan Stone Morris <21117@bridgend.co.uk>
2021-02-19 11:37:46 +0000

    I need a bit of help. I'm  getting "ModelNotFoundError"

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,18 @@
+import PaymentCard
+
 def main():
     #write your code below this line
+   def test_exercise(Bananas):
+    card = PaymentCard(30)
+    print(card)
+
+    out, err = Bananas.readouterr()
+    assert out == "The card has a balance of 30 pounds\n"
+
+    card = PaymentCard(30)
+    card.add_money(15)
+    print(card)
+
 
 if __name__ == '__main__':
     main()
